89e8a1dd92eab946deebb22f3b49ec0e605f9b86
laura.souza <laura.souza@gclaims.com.br>
2023-06-29 00:54:36 -0300

    ajuste

--- projeto_c214_l1/src/components/Sidebar/index.js
+++ projeto_c214_l1/src/components/Sidebar/index.js
@@ -1,7 +1,3 @@
-import { Link } from 'react-router-dom';
-import { FaRegListAlt } from "react-icons/fa";
-import { AiFillHome, AiOutlineSearch } from "react-icons/ai";
-import listContacts from '../../assets/list-contacts.png';
 import './style.css';
 
 export default function Sidebar() {
